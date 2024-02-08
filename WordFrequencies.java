25b24ea9521818dc7ca232f4733f777d75d1d837
Vaishali Vyas <sayvilahsiav@outlook.com>
2024-02-08 17:05:50 +0530

    Implemented findUnique method

--- WordFrequencies.java
+++ WordFrequencies.java
@@ -1,4 +1,5 @@
 import java.util.*;
+import edu.duke.*;
 
 /**
  * Write a description of WordFrequencies here.
@@ -10,10 +11,34 @@ public class WordFrequencies {
     private ArrayList<String> myWords;
     private ArrayList<Integer> myFreqs;
     
-    public void WordFrequencies() {
-        myWords = new ArrayList<>();
-        myFreqs = new ArrayList<>();
+    public WordFrequencies() {
+        myWords = new ArrayList<String>();
+        myFreqs = new ArrayList<Integer>();
     }
     
+    private void findUnique() {
+        myWords.clear();
+        myFreqs.clear();
+        
+        FileResource res = new FileResource();
+        for (String s : res.words()) {
+            s = s.toLowerCase();
+            int index = myWords.indexOf(s);
+            if(index == -1) {
+                myWords.add(s);
+                myFreqs.add(1);
+            } else {
+                int value = myFreqs.get(index);
+                myFreqs.set(index, value + 1);
+            }
+        }
+    }
+    
+    public void testFindUnique() {
+        findUnique();
+        for(int k=0; k < myWords.size(); k++){
+            System.out.println(myFreqs.get(k) + "\t" + myWords.get(k));
+        }
+    }
 
 }
