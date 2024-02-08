08527b7c35b8ee7d3ff12f3c962abdde3f1793b3
Vaishali Vyas <sayvilahsiav@outlook.com>
2024-02-08 16:29:48 +0530

    Created class WordFrequencies

---
+++ WordFrequencies.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+/**
+ * Write a description of WordFrequencies here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class WordFrequencies {
+    private ArrayList<String> myWords;
+    private ArrayList<Integer> myFreqs;
+    
+    public void WordFrequencies() {
+        myWords = new ArrayList<>();
+        myFreqs = new ArrayList<>();
+    }
+    
+
+}
