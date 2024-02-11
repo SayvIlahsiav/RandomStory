aab1b82ea5dca6f7f2bb83436333234e49b179c0
Vaishali Vyas <sayvilahsiav@outlook.com>
2024-02-11 17:23:44 +0530

    Created class CharactersInPlay

---
+++ CharactersInPlay.java
@@ -0,0 +1,15 @@
+
+/**
+ * Write a description of CharactersInPlay here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class CharactersInPlay {
+    
+    
+    public static void main(String[] args) {
+        CharactersInPlay cip = new CharactersInPlay();
+    }
+
+}
