562801a7d6eb3183d07d3424ea24648d15e47a1b
Vaishali Vyas <sayvilahsiav@outlook.com>
2024-02-11 17:26:47 +0530

    Added class constructor

--- CharactersInPlay.java
+++ CharactersInPlay.java
@@ -1,3 +1,4 @@
+import java.util.*;
 
 /**
  * Write a description of CharactersInPlay here.
@@ -6,7 +7,13 @@
  * @version (a version number or a date)
  */
 public class CharactersInPlay {
+    private ArrayList<String> charNames;
+    private ArrayList<Integer> charCounts;
     
+    public CharactersInPlay() {
+        charNames = new ArrayList<String>();
+        charCounts = new ArrayList<Integer>();
+    }
     
     public static void main(String[] args) {
         CharactersInPlay cip = new CharactersInPlay();
